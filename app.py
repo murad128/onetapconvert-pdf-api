4c3bce1ec7adb0623393d11b3ed89111bcd9212c
OneTapConvert <mmustafayev232@gmail.com>
2026-03-29 10:42:35 +0000

    Fix: add json import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, io, base64, threading, time, urllib.request
+import os, io, base64, json, threading, time, urllib.request
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
